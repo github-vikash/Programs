2d5adbd42916a14e57cb45fab9fd35840c76de07
vikash kumar <k.vikash2@partner.samsung.com>
2019-07-24 16:37:42 +0530

    word_count_in_string

--- word_count_in_string.js
+++ word_count_in_string.js
@@ -11,6 +11,9 @@ function word_count(str){
     console.log(data)
 }
 
+word_count("vikash kumar sing vikash kuma singh")
+// output { vikash: 2, kumar: 1, sing: 1, kuma: 1, singh: 1 }
+
 function given_word_count(str,word){
     let splitedword=[]=str.split(" ");
     let count=0;
@@ -19,8 +22,9 @@ function given_word_count(str,word){
             count +=1;
         }
     }
-    console.log(word,'present in ',str  ,'is',count);
+    console.log(word,':',count);
 }
 
 given_word_count("vikash kumar sing vikash kuma singh","vikash");
-word_count("vikash kumar sing vikash kuma singh");
+
+//output vikash : 2
